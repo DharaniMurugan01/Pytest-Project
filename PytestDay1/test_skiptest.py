1dd718055d045ed57fba1f0560f8b8515b14ebdc
DharaniMurugan01 <2k21dhara@gmail.com>
2025-05-14 08:02:00 +0530

    Update test_skiptest.py

--- PytestDay1/test_skiptest.py
+++ PytestDay1/test_skiptest.py
@@ -3,15 +3,16 @@ import sys
 
 def test_sample_one():
     print("hii")
+
 def test_sample_two():
-    a=10
-    b=10
-    assert a==b
+    a = 10
+    b = 10
+    assert a == b
+
 def test_sample_three():
-    a=10
-    b=20
-    assert a<b
-import pytest
+    a = 10
+    b = 20
+    assert a < b
 
 @pytest.mark.skip(reason="Not ready yet")
 def test_feature():
@@ -19,9 +20,9 @@ def test_feature():
 
 @pytest.mark.xfail(reason="Bug not fixed")
 def test_known_bug():
-    a=10
-    b=20
-    assert a==b
+    a = 10
+    b = 20
+    assert a == b
 
 @pytest.mark.skipif(sys.platform == "win32", reason="Not supported on Windows")
 def test_linux_feature():
@@ -29,4 +30,4 @@ def test_linux_feature():
 
 @pytest.mark.skip(reason="wrong word")
 def test_failes():
-    assert 1==1
+    assert 1 == 1
